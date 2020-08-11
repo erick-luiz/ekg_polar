ff312c6768f7424d88bfaea69944f99e16b6c044
Erick Luiz Fonseca Lopes <902139@lojasrenner.com.br>
2020-08-11 00:41:00 -0300

    Insert bundle.min.js

--- minifier.js
+++ minifier.js
@@ -1,6 +1,6 @@
 var minifier = require('minifier')
 
-var input =  ['./build/Main.js',]
+var input =  ['./build/Main.js']
 
 minifier.on('error', function(err) {
 	console.log('Error in Js Files Minifier!')
